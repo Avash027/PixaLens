5d2a8ba258bcd2add2496a38e7bb0a0e5a5667bb
unknown <avashmitra2706@gmail.com>
2021-01-13 11:33:28 +0530

    Minor changes to UI

--- src/util/Appbar.js
+++ src/util/Appbar.js
@@ -3,9 +3,10 @@ import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -22,10 +23,53 @@ const useStyles = makeStyles((theme) => ({
 export default function Appbar() {
   const classes = useStyles();
 
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = (event) => {
+    const id = event.target.id;
+    if (id === "0")
+      window.open("https://linkedin.com/in/avash-mitra-4548761a6/");
+    else if (id === "1") window.open("https://github.com/Avash027/PixaLens");
+    else window.open("https://github.com/Avash027");
+    setAnchorEl(null);
+  };
+
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+            aria-haspopup="true"
+            onClick={handleClick}
+          >
+            <MenuIcon />
+          </IconButton>
+
+          <Menu
+            id="menu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem onClick={handleClose} id="0">
+              My profile
+            </MenuItem>
+            <MenuItem onClick={handleClose} id="1">
+              Github Repo
+            </MenuItem>
+            <MenuItem onClick={handleClose} id="2">
+              Github Profile
+            </MenuItem>
+          </Menu>
           <Typography variant="h6" className={classes.title}>
             PixaLens
           </Typography>
